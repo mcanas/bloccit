f7ba4c4c7bcdb1842a064488a09acc49c9973f71
Miguel Canas <mcanas1981@gmail.com>
2015-08-31 16:23:12 -0400

    Checkpoint 30 complete, bloccit seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+include RandomData
+
+50.times do
+  Post.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+
+posts = Post.all
+
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+
+Post.find_or_create_by( title: 'This is a unique post', body: 'It will only be created once' )
