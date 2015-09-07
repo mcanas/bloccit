e5f62d44cdd7b632fc551a0f5de99f14c452889e
Miguel Canas <mcanas1981@gmail.com>
2015-09-07 12:08:28 -0400

    assignment 31 complete, bloccit intro to crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    i = 0
+    @posts = censor_posts(Post.all)
   end
 
   def show
@@ -10,5 +11,16 @@ class PostsController < ApplicationController
   end
 
   def edit
+  end
+
+  def censor_posts(posts)
+    if posts
+      i = 0
+      posts.each do |post|
+        i += 1
+        post.title = 'CENSORED' if i % 5 == 0
+      end
+    end
+    posts
   end
 end
