b48ce757866b1e9e5d30aa344345b950556ed7b9
Miguel Canas <mcanas1981@gmail.com>
2015-10-11 21:22:38 -0400

    checkpoint complete, bloccit authentication signing in

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   post 'confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
