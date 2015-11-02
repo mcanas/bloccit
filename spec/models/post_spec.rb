f2c673c4181c1779eb0e44d4d1ac505997854b27
Miguel Canas <mcanas1981@gmail.com>
2015-11-01 23:17:44 -0500

    checkpoint complete, bloccit comments

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Post, type: :model do
 
   it { should belong_to(:topic) }
   it { should belong_to(:user) }
+  it { should have_many(:comments) }
 
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
