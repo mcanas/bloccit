18e2c79705c3da1923b108ab2eba36b3c4234e0f
Miguel Canas <mcanas1981@gmail.com>
2016-01-10 15:30:03 -0500

    assignment complete, bloccit building an api, sending data

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -17,4 +17,43 @@ class Api::V1::PostsController < Api::V1::BaseController
     post = Post.find(params[:id])
     render json: post.to_json(include: :comments), status: 200
   end
+
+  def update
+    post = Post.find(params[:id])
+
+    if post.update_attributes(post_params)
+      render json: post.to_json, status: 201
+    else
+      render json: { error: 'Post update failed', status: 400 }, status: 400
+    end
+  end
+
+  def create
+    topic = Topic.find(params[:topic_id])
+    post = topic.posts.build(post_params)
+    post.user = @current_user
+
+    if post.valid?
+      post.save!
+      render json: post.to_json, status: 201
+    else
+      render json: { error: 'Post is invalid', status: 400 }, status: 400
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+
+    if post.destroy
+      render json: { message: 'Post destroyed', status: 200 }, status: 200
+    else
+      render json: { error: 'Post destoy failed', status: 400 }, status: 400
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
